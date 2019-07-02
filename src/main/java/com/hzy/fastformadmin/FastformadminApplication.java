f0e674e5ae4c3575f70a8ddc6e09ac2eeaa42aa4
jc814 <1054539066@qq.com>
2019-07-02 23:33:26 +0800

    Initial commit

---
+++ src/main/java/com/hzy/fastformadmin/FastformadminApplication.java
@@ -0,0 +1,13 @@
+package com.hzy.fastformadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastformadminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastformadminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hzy/fastformadmin/FastformadminApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hzy.fastformadmin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FastformadminApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
