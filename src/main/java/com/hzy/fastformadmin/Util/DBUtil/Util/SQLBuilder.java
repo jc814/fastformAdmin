808419a369dfb49df54514468ae4def973860ee1
hzy <1054539066@qq.com>
2019-07-07 22:55:32 +0800

    构建框架

--- src/main/java/com/hzy/fastformadmin/Util/DBUtil/Util/SQLBuilder.java
+++ src/main/java/com/hzy/fastformadmin/Util/DBUtil/Util/SQLBuilder.java
@@ -1,15 +1,18 @@
 package com.hzy.fastformadmin.Util.DBUtil.Util;
 
 import com.hzy.fastformadmin.Util.DBUtil.annotation.Ignore;
-import com.sun.javafx.binding.StringFormatter;
+import com.hzy.fastformadmin.Util.DBUtil.annotation.Key;
 
 import java.lang.reflect.Field;
 import java.text.MessageFormat;
+import java.util.List;
+
 /*
  * 实现基本的sql语句
  */
 public class SQLBuilder {
-    public <T> StringBuilder updateBuild(Class<T> tClass, String option) {
+
+    public <T> StringBuilder baseSqlBuild(Class<T> tClass, String option) {
         String tableName = ClassUtil.GetTableName(tClass);
         String KeyName = ClassUtil.GetkeyfieldName(tClass);
         StringBuilder sql = new StringBuilder();
@@ -41,8 +44,8 @@ public class SQLBuilder {
             case "update":
                 sql.append(MessageFormat.format("update {0} set ", tableName));
                 for (Field field : fields) {
-                    if (field.getDeclaredAnnotation(Ignore.class) == null) {
-                        column.append("," + field.getName() + " = ? ");
+                    if (field.isAnnotationPresent(Ignore.class) || field.isAnnotationPresent(Key.class)) {
+                        column.append(",").append(field.getName()).append(" = ? ");
                     }
                 }
                 column.deleteCharAt(0);
@@ -53,6 +56,71 @@ public class SQLBuilder {
                 sql.append(MessageFormat.format("delete from {0} ", tableName));
                 sql.append(MessageFormat.format(" where {0}=?",KeyName));
                 break;
+            case "select":
+                sql.append(MessageFormat.format("select * from {0} where ", tableName));
+                String keyName = ClassUtil.GetkeyfieldName(tClass);
+                sql.append(keyName).append(" = ? ");
+
+        }
+        return sql;
+    }
+    public StringBuffer updateParamSqlBuild(List<String> columns,List<String> wheres,String tableName) {
+        StringBuffer sql = new StringBuffer();
+        sql.append(MessageFormat.format("update {0} set ", tableName));
+        int i = 0;
+        int j = 0;
+        for(String column : columns){
+            if(i != 0){
+                sql.append(",");
+            }
+            i++;
+            sql.append(column).append(" = ").append(" ? ");
+        }
+        sql.append(" where ");
+        for(String where : wheres){
+            if(j != 0){
+                sql.append(" and ");
+            }
+            j++;
+            sql.append(where).append(" = ").append(" ? ");
+        }
+        return sql;
+    }
+    public StringBuffer deleteParamSqlBuild(List<String> wheres,String tableName) {
+        StringBuffer sql = new StringBuffer();
+        sql.append(MessageFormat.format("delete from {0} ", tableName));
+        sql.append(" where ");
+        int j = 0;
+        for(String where : wheres){
+            if(j != 0){
+                sql.append(" and ");
+            }
+            j++;
+            sql.append(where).append(" = ").append(" ? ");
+        }
+        return sql;
+    }
+    public StringBuffer selectParamSqlBuild(List<String> columns,List<String> wheres,String tableName,String type) {
+        StringBuffer sql = new StringBuffer();
+        sql.append("select ");
+        int i = 0;
+        int j = 0;
+        if("param".equals(type)){
+            for(String column : columns){
+                if(i != 0){
+                    sql.append(" , ");
+                }
+                i++;
+                sql.append(column);
+            }
+        }
+        sql.append(MessageFormat.format(" from {0} ", tableName));
+        for(String where : wheres){
+            if(j != 0){
+                sql.append(" and ");
+            }
+            j++;
+            sql.append(where).append(" = ").append(" ? ");
         }
         return sql;
     }
--- src/main/java/com/hzy/fastformadmin/Util/DBUtil/base/PreCommand.java
+++ src/main/java/com/hzy/fastformadmin/Util/DBUtil/base/PreCommand.java
@@ -2,9 +2,12 @@ package com.hzy.fastformadmin.Util.DBUtil.base;
 
 import com.hzy.fastformadmin.Util.DBUtil.Util.ClassUtil;
 import com.hzy.fastformadmin.Util.DBUtil.Util.SQLBuilder;
+import com.hzy.fastformadmin.Util.DBUtil.annotation.Ignore;
+import com.hzy.fastformadmin.Util.DBUtil.annotation.Key;
+import com.hzy.fastformadmin.Util.DBUtil.annotation.TableName;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.lang.reflect.Field;
+import java.util.*;
 
 public class PreCommand {
     private SQLBuilder sqlBuilder;
@@ -14,7 +17,7 @@ public class PreCommand {
         }
     }
     public <T> Command initDelete(T entity){
-        StringBuilder sql = sqlBuilder.updateBuild(entity.getClass(),"delete");
+        StringBuilder sql = sqlBuilder.baseSqlBuild(entity.getClass(),"delete");
         Command command = CommandFactory.getCommand();
         command.setSQLText(sql.toString());
         List<Object> param = new ArrayList<Object>();
@@ -24,10 +27,93 @@ public class PreCommand {
     }
 
     public <T> Command initInsert(T entity){
-        StringBuilder sql = sqlBuilder.updateBuild(entity.getClass(),"insert");
+        StringBuilder sql = sqlBuilder.baseSqlBuild(entity.getClass(),"insert");
         Command command = CommandFactory.getCommand();
         command.setSQLText(sql.toString());
         command.setSQLParams(ClassUtil.EntityValueToArray(entity));
         return command;
+    }
+
+    public <T> Command initUpdate(T entity){
+        StringBuilder sql = sqlBuilder.baseSqlBuild(entity.getClass(),"update");
+        List<Object> param = new ArrayList<Object>();
+        Field[] fields = entity.getClass().getDeclaredFields();
+        try {
+            for (Field field : fields) {
+                if (field.isAnnotationPresent(Ignore.class) || field.isAnnotationPresent(Key.class)) {
+                    param.add(field.get(entity));
+                }
+            }
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        Command command = CommandFactory.getCommand();
+        command.setSQLText(sql.toString());
+        command.setSQLParams(param);
+        return command;
+    }
+
+    public <T> Command initSelectOne(Class<T> tClass,String primaryKey){
+        StringBuilder sql = sqlBuilder.baseSqlBuild(tClass,"select");
+        List<Object> param = new ArrayList<>();
+        param.add(primaryKey);
+        Command command = CommandFactory.getCommand();
+        command.setSQLText(sql.toString());
+        command.setSQLParams(param);
+        return command;
+    }
+
+    public <T> Command initUpdOrFindMapByParam(String tableName,Map<String,Object> columnMap,Map<String,Object> whereMap,String type){
+        List<String> columnKeys ;
+        List<Object> columnValues ;
+        List<String> whereKeys ;
+        List<Object> whereValues ;
+        StringBuffer sql = new StringBuffer();
+        columnKeys = (List<String>)mapToList(columnMap).get("key");
+        columnValues = (List<Object>)mapToList(columnMap).get("values");
+        whereKeys = (List<String>)mapToList(whereMap).get("key");
+        whereValues = (List<Object>)mapToList(whereMap).get("values");
+        if("select".equals(type)){
+            sql = sqlBuilder.selectParamSqlBuild(columnKeys,whereKeys,tableName,"param");
+        }else{
+            sql = sqlBuilder.updateParamSqlBuild(columnKeys,whereKeys,tableName);
+        }
+        Command command = CommandFactory.getCommand();
+        command.setSQLText(sql.toString());
+        columnValues.addAll(whereValues);
+        command.setSQLParams(columnValues);
+        return command;
+    }
+
+    public <T> Command initDelOrFindObjByParam(String tableName,Map<String,Object> whereMap){
+        List<String> whereKeys ;
+        List<Object> whereValues ;
+        whereKeys = (List<String>)mapToList(whereMap).get("key");
+        whereValues = (List<Object>)mapToList(whereMap).get("values");
+        StringBuffer sql = new StringBuffer();
+        if("select".equals("type")){
+            sql = sqlBuilder.deleteParamSqlBuild(whereKeys,tableName);
+        }else{
+            sql = sqlBuilder.selectParamSqlBuild(null,whereKeys,tableName,"all");
+        }
+        Command command = CommandFactory.getCommand();
+        command.setSQLText(sql.toString());
+        command.setSQLParams(whereValues);
+        return command;
+    }
+
+    private Map<String,Object> mapToList(Map<String,Object> map){
+        Map<String,Object> result = new HashMap<>();
+        List<String> keys = new ArrayList<>();
+        List<Object> values = new ArrayList<>();
+        Iterator entries = map.entrySet().iterator();
+        while (entries.hasNext()) {
+            Map.Entry columnEntry = (Map.Entry)entries.next();
+            keys.add(columnEntry.getKey().toString());
+            values.add(columnEntry.getValue());
+        }
+        result.put("keys",keys);
+        result.put("values",values);
+        return result;
     }
 }
